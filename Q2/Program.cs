932d7db08354711968d7844fe4c858a0d860e0bc
ispan <ispan@DESKTOP-PAKSETB>
2022-10-19 16:48:45 +0800

    加入專案檔案。

---
+++ Q2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sentence = "我是一個\"好學生\".";
+            Console.WriteLine(sentence);
+        }
+    }
+}
---
+++ Q3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string value = @"我是第一行
+我是第二行";
+            Console.WriteLine(value);
+        }
+    }
+}
